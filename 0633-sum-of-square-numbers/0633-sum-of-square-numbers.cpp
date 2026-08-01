9eb35c4082fd83d83e92318b1e6a0a463259dbd9
Supriya Kumari <supriyadecodes@gmail.com>
2026-08-02 00:50:00 +0530

    Time: 3 ms (31.88%), Space: 7.8 MB (57.52%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isperfect(int n) {
+        int root = sqrt(n);
+        if (root * root == n)
+            return true;
+        else
+            return false;
+    }
+    bool judgeSquareSum(int c) {
+        int x = 0;
+        int y = c;
+        while (x <= y) {
+            if (isperfect(x) && isperfect(y)) {
+                return true;
+            }
+            else if (!isperfect(y)) {
+                    y = (int)sqrt(y) * (int)sqrt(y);
+                    x = c - y;
+                }
+            else {
+                    x = ((int)sqrt(x) + 1) *( (int)sqrt(x) + 1);
+                    y = c - x;
+            }
+        }
+        return false;
+    }
+};
